2838c253c8497f85d646fa25bfa8ca5c8efb7ff1
jones-py <jones03chimfwembe@gmail.com>
2021-02-08 08:52:22 +0000

    final push

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -7,48 +7,13 @@ Devise.setup do |config|
   config.secret_key = 'b8482b4d4d6c11be50e74b9897948db362a5fc45bd42f906871f81b63bcf1e18dccb5218532cc1b80c0521b44d4002d9b5f763d84c0d9f98227e1d0cfdc69a9e'
   # config.secret_key = 'a9c1e69fa9d38b5d305b7a41ec9d88b39ee7182dd1dcd13aae08e9603dece0d073c4cb6ebf32e9ef41ace4f5313ba2df577183139013421de227042a51048f07'
 
-  # ==> Controller configuration
-  # Configure the parent class to the devise controllers.
-  # config.parent_controller = 'DeviseController'
 
-  # ==> Mailer Configuration
-  # Configure the e-mail address which will be shown in Devise::Mailer,
-  # note that it will be overwritten if you use your own mailer class
-  # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
-  # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
 
-  # Configure the parent class responsible to send e-mails.
-  # config.parent_mailer = 'ActionMailer::Base'
-
-  # ==> ORM configuration
-  # Load and configure the ORM. Supports :active_record (default) and
-  # :mongoid (bson_ext recommended) by default. Other ORMs may be
-  # available as additional gems.
   require 'devise/orm/active_record'
 
-  # ==> Configuration for any authentication mechanism
-  # Configure which keys are used when authenticating a user. The default is
-  # just :email. You can configure it to use [:username, :subdomain], so for
-  # authenticating a user, both parameters are required. Remember that those
-  # parameters are used only when authenticating and not when retrieving from
-  # session. If you need permissions, you should implement that in a before filter.
-  # You can also supply a hash where the value is a boolean determining whether
-  # or not authentication should be aborted when the value is not present.
-  # config.authentication_keys = [:email]
-
-  # Configure parameters from the request object used for authentication. Each entry
-  # given should be a request method and it will automatically be passed to the
-  # find_for_authentication method and considered in your model lookup. For instance,
-  # if you set :request_keys to [:subdomain], :subdomain will be used on authentication.
-  # The same considerations mentioned for authentication_keys also apply to request_keys.
-  # config.request_keys = []
-
-  # Configure which authentication keys should be case-insensitive.
-  # These keys will be downcased upon creating or modifying a user and when used
-  # to authenticate or find a user. Default is :email.
+  
   config.case_insensitive_keys = [:email]
 
   # Configure which authentication keys should have whitespace stripped.
